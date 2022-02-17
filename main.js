49b683724b4c4bc7eaa6d687e91f51284c7d724d
BenWatts9 <brandolphwatts@gmail.com>
2022-02-17 11:28:55 -0600

    initial commit for calculator exercise

---
+++ main.js
@@ -0,0 +1,28 @@
+const add = (firstNumber, secondNumber) => {
+    const sum = firstNumber + secondNumber
+
+    return sum
+}
+
+const subtract = (firstNumber, secondNumber) => {
+    const sum = firstNumber - secondNumber
+
+    return sum
+}
+
+const multiply = (firstNumber, secondNumber) => {
+    const sum = firstNumber * secondNumber
+
+    return sum
+}
+
+const divide = (firstNumber, secondNumber) => {
+    const sum = firstNumber / secondNumber
+
+    return sum
+}
+
+//Animal leg problem
+
+const answer = divide(60,2.5)
+console.log(`There are ${answer} animals on the farm.`)
